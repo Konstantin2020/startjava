085bee048c552ddd6fc7253deed3a5133f46429c
Konstantin2020 <ykn79@mail.ru>
2021-05-17 18:04:56 +0500

    implement HW_1

--- Lesson 1/Variable.java
+++ Lesson 1/Variable.java
@@ -1,5 +1,21 @@
-public class Variable{
-    public static void main(String[] args){
-    System.out.println("Hello, world");
+public class Variable {
+    public static void main(String[] args) {
+        byte coreCPU = 2;
+        short ram = 8;
+        int quantityHdd = 2;
+        long totalHddCapacity=627502022656l;
+        float frequencyCPU = 2.60f;
+        double monitorDiagonal = 15.6d;
+        char cpuLetterIndex = 'i';
+        boolean notebook = true;
+        System.out.println("coreCPU = " + coreCPU);
+        System.out.println("ram = " + ram);
+        System.out.println("quantityHdd = " + quantityHdd);
+        System.out.println("totalHddCapacity = " + totalHddCapacity);
+        System.out.println("frequencyCPU = " + frequencyCPU);
+        System.out.println("monitorDiagonal = " + monitorDiagonal);
+        System.out.println("cpuLetterIndex = " + cpuLetterIndex);
+        System.out.println("notebook = " + notebook);
+
     }
 }
