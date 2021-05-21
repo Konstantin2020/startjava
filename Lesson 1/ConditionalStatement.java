100676ba5540e8207fbd2641b8bae6eabd258e77
Konstantin2020 <ykn79@mail.ru>
2021-05-21 23:57:22 +0500

    implement HW1

--- Lesson 1/ConditionalStatement.java
+++ Lesson 1/ConditionalStatement.java
@@ -1,23 +1,26 @@
 public class ConditionalStatement {
     public static void main(String[] args) {
         int age = 29;
-        boolean isMaleGender = true;
-        float height = 1.81f;
-        char firstLetterOfName = 'K';
         if (age < 20) {
             System.out.println("Возраст" + age + " больше 20");
         }
+
+        boolean isMaleGender = true;
         if (isMaleGender) {
             System.out.println("Мужского пола");
         }
         if (!isMaleGender) {
             System.out.println("Женского пола");
         }
+
+        float height = 1.81f;
         if (height < 1.80) {
             System.out.println("Низкого или среднего роста");
         } else {
             System.out.println("Среднего или высокого роста");
         }
+
+        char firstLetterOfName = 'K';
         if (firstLetterOfName == 'M') {
             System.out.println("Первая буква имени М");
         } else if (firstLetterOfName == 'I'){
--- Lesson 1/Variable.java
+++ Lesson 1/Variable.java
@@ -1,21 +1,27 @@
 public class Variable {
     public static void main(String[] args) {
         byte coreCPU = 2;
-        short ram = 8;
-        int quantityHdd = 2;
-        long totalHddCapacity=627502022656l;
-        float frequencyCPU = 2.60f;
-        double monitorDiagonal = 15.6d;
-        char cpuLetterIndex = 'i';
-        boolean mobile = true;
         System.out.println("coreCPU = " + coreCPU);
-        System.out.println("ram = " + ram);
-        System.out.println("quantityHdd = " + quantityHdd);
-        System.out.println("totalHddCapacity = " + totalHddCapacity);
-        System.out.println("frequencyCPU = " + frequencyCPU);
-        System.out.println("monitorDiagonal = " + monitorDiagonal);
-        System.out.println("cpuLetterIndex = " + cpuLetterIndex);
-        System.out.println("mobile = " + mobile);
 
+        short ram = 8;
+        System.out.println("ram = " + ram);
+
+        int quantityHdd = 2;
+        System.out.println("quantityHdd = " + quantityHdd);
+
+        long totalHddCapacity = 627502022656l;
+        System.out.println("totalHddCapacity = " + totalHddCapacity);
+
+        float frequencyCPU = 2.60f;
+        System.out.println("frequencyCPU = " + frequencyCPU);
+
+        double monitorDiagonal = 15.6d;
+        System.out.println("monitorDiagonal = " + monitorDiagonal);
+
+        char letterIndexOfCPU = 'i';
+        System.out.println("letterIndexOfCPU = " + letterIndexOfCPU);
+
+        boolean mobile = true;
+        System.out.println("mobile = " + mobile);
     }
 }
